e6ac9fb345405bf7c873d072c0aa7cadddf94743
Adrien NEYRON <adrien.neyron@gmail.com>
2024-03-01 17:11:26 +0100

    MAJ

--- src/components/forms/TwoColContactUsWithIllustrationFullForm.js
+++ src/components/forms/TwoColContactUsWithIllustrationFullForm.js
@@ -26,7 +26,7 @@ const Subheading = tw(SubheadingBase)`text-center md:text-left`;
 const Heading = tw(SectionHeading)`mt-4 font-black text-left text-3xl sm:text-4xl lg:text-5xl text-center md:text-left leading-tight`;
 const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:text-lg font-medium leading-relaxed text-secondary-100`
 
-const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col max-w-sm mx-auto md:mx-0`
+const Form = tw.form`mt-8 md:mt-10 text-sm grid grid-cols-2 gap-x-8 max-w-lg mx-auto md:mx-0`
 const Input = tw.input`mt-6 first:mt-0 border-b-2 py-3 focus:outline-none font-medium transition duration-300 hocus:border-primary-500`
 const Textarea = styled(Input).attrs({as: "textarea"})`
   ${tw`h-24`}
