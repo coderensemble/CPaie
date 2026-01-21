106e1396d5350dd9bbff68500dd4510449eab377
Adrien NEYRON <adrien.neyron@gmail.com>
2026-01-21 11:20:26 +0100

    Restaure API_URL in useAPI

--- cpaie_frontend/src/hooks/useApi.ts
+++ cpaie_frontend/src/hooks/useApi.ts
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 import { useCallback } from "react";
 
-const API_URL = "/api";
+const API_URL = process.env.REACT_APP_API_URL || "/api";
 
 export const useApi = () => {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
