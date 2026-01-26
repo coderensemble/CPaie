893f612d1957ff4c1020d920d64cd102af7cd1e5
Adrien NEYRON <adrien.neyron@gmail.com>
2026-01-26 14:03:16 +0100

    add credentials in axios requete

--- cpaie_backend/src/routes/auth.routes.ts
+++ cpaie_backend/src/routes/auth.routes.ts
@@ -9,13 +9,22 @@ const router = Router();
 // OPTIONS preflight pour ce router
 router.options('*', cors());
 
-router.get(
-  '/me',
-  checkJwt,
-  syncOrCreateUser,
-  (req: AuthRequest, res: Response) => {
-    res.json(req.user);
-  }
-);
+const allowedOrigins = [
+  'https://cpluspaie.vercel.app',
+  'https://admin.cpluspaie.vercel.app',
+];
+
+const corsOptions = {
+  origin: function (origin: string | undefined, callback: any) {
+    if (!origin || allowedOrigins.includes(origin)) callback(null, true);
+    else callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true,
+};
+
+router.options('*', cors(corsOptions)); // préflight OPTIONS
+router.get('/me', cors(corsOptions), checkJwt, syncOrCreateUser, (req: AuthRequest, res: Response) => {
+  res.json(req.user);
+});
 
 export default router;
